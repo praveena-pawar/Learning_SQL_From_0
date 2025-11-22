f3a572f6ce6e6807979f54693e1931f752e4c4f2
praveena-pawar <praveena.dev02@gmail.com>
2025-11-22 16:03:01 +0530

    removed having

--- level4_group_by.sql
+++ level4_group_by.sql
@@ -32,13 +32,3 @@ GROUP BY grade;
 
 
 
--- Show only cities that have more than 2 students.
- SELECT city FROM students 
-GROUP BY city
-HAVING COUNT(city) > 2;
-
-
--- Show grades where the average marks is greater than 85.
-SELECT grade, AVG(marks) FROM students
-GROUP BY grade 
-HAVING AVG(marks) > 85;
