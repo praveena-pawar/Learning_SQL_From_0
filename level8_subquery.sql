9aea74cf31a354c69752a9e1c550f0d48c8cd241
praveena-pawar <praveena.dev02@gmail.com>
2025-11-25 19:56:11 +0530

    LEVEL 8 — Added new query

--- level8_subquery.sql
+++ level8_subquery.sql
@@ -6,3 +6,12 @@
 -- Show students who have marks greater than the average marks.
 SELECT * FROM students 
 WHERE marks > (SELECT AVG(marks) FROM students);
+
+
+
+
+
+-- Show students older than the average age.
+SELECT *
+FROM students
+WHERE age > (SELECT AVG(age) FROM students);
